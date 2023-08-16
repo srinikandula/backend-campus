c75919adb1735babe856c44cf229796f50bba917
srinikandula <srini.kandula@whizzard.in>
2023-08-16 10:07:36 +0530

    Remove unwanted files

--- src/main/java/com/anyaudit/db2/repository/ClientsRepostory.java
+++
@@ -1,9 +0,0 @@
-//package com.anyaudit.db2.repository;
-//
-//import org.springframework.data.jpa.repository.Query;
-//
-//public class ClientsRepostory {
-//
-//    @Query(value = "SELECT * FROM `sms_clients` WHERE status = 'active'", nativeQuery = true)
-//    Long getClientCount();
-//}
