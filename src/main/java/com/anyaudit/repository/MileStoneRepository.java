aac5ff3210e4e324b32436534562be2322387d23
srinikandula <srini.kandula@whizzard.in>
2023-08-17 18:03:51 +0530

    Added model and service for Milestone and also added a test case

--- src/main/java/com/anyaudit/repository/MileStoneRepository.java
+++ src/main/java/com/anyaudit/repository/MileStoneRepository.java
@@ -1,14 +1,10 @@
 package com.anyaudit.repository;
 
-import java.util.Optional;
-
+import com.anyaudit.models.Milestone;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.anyaudit.models.ERole;
-import com.anyaudit.models.Role;
-
 @Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-  Optional<Role> findByName(ERole name);
+public interface MileStoneRepository extends JpaRepository<Milestone, Long> {
+
 }
